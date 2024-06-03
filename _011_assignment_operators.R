f6f63751a884d5c38b1f6b27b36ead1f1e356150
raghavtwenty <raghavtwenty@gmail.com>
2024-06-03 08:21:12 +0530

    Added all assignment operators

---
+++ _011_assignment_operators.R
@@ -0,0 +1,24 @@
+# Filename: _011_assignment_operators.R
+# Title: List of all assignment operators in R
+# Author: Raghava | GitHub : @raghavtwenty
+# Date Created: June 3, 2024 | Last Updated: June 3, 2024
+# Language: R | Version: 4.4.0
+
+
+# Assignment Operations
+
+# Leftward assignment
+input <- 5
+print(paste("Leftward assignment (<-):",input))
+
+# Leftward assignment - not recommended
+input  = 6
+print(paste("Leftward assignment (=):",input))
+
+# Rightward assignment
+8  -> input
+print(paste("Rightward assignment (->):",input))
+
+# Global assignment
+input  <<- 10
+print(paste("Global assignment (<<-):",input))
